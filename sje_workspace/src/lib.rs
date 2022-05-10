890dc361cd16d8cdd9b74effb1990424465db70b
Shuto Shikama <@gmail.com>
2022-05-10 14:22:49 +0900

    My First Commit

---
+++ sje_workspace/src/lib.rs
@@ -0,0 +1,31 @@
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    #[derive(sje_immutable_macro::Immutable, Default)]
+    struct TestData {
+        #[allow(dead_code)]
+        int_value: i32,
+        #[allow(dead_code)]
+        float_value: f32,
+        #[allow(dead_code)]
+        uint_value: u32,
+    }
+
+    #[test]
+    fn generate_test() {
+        let test_data = Arc::new(TestData::default());
+        let new_instance = test_data.with_float_value(0.1).with_uint_value(77);
+        let new_new_instance = test_data.with_float_value(0.1).with_uint_value(5);
+        assert_eq!(test_data.float_value, Default::default());
+        assert_eq!(new_instance.float_value, 0.1);
+        assert_eq!(new_instance.uint_value, 77);
+        assert_eq!(new_new_instance.uint_value, 5);
+    }
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
