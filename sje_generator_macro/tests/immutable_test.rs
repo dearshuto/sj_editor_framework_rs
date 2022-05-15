a91fb5dcac38e736b44a92f72063f5948df7c55b
Shuto Shikama <@gmail.com>
2022-05-15 22:56:38 +0900

    ソース自動生成マクロのテストを追加

---
+++ sje_generator_macro/tests/immutable_test.rs
@@ -0,0 +1,66 @@
+use std::sync::Arc;
+
+use sje_generator_macro::Immutable;
+
+#[derive(Immutable)]
+struct StructData {
+    pub int_value: i32,
+    pub uint_value: u32,
+}
+
+#[derive(Immutable)]
+struct TestData {
+    pub struct_data: Arc<StructData>,
+}
+
+#[derive(Immutable)]
+struct GenericData<T: Clone> {
+    generic_value: T,
+    arc_value: Arc<StructData>,
+}
+
+#[test]
+fn simple_test() {
+    let struct_data = StructData {
+        int_value: 12,
+        uint_value: 24,
+    };
+    let new_int_value_struct_data = struct_data.with_int_value(7);
+    let new_uint_value_struct_data = new_int_value_struct_data.with_uint_value(23);
+
+    assert_eq!(new_int_value_struct_data.int_value, 7);
+    assert_eq!(new_uint_value_struct_data.int_value, 7);
+    assert_eq!(new_uint_value_struct_data.uint_value, 23);
+}
+
+#[test]
+fn with_struct_test() {
+    let struct_data = StructData {
+        int_value: 7,
+        uint_value: 11,
+    };
+    let new_struct_data = struct_data.with_int_value(33);
+    let test_data = TestData {
+        struct_data: Arc::new(struct_data),
+    };
+
+    let new_test_data = test_data.with_struct_data(new_struct_data);
+    assert_eq!(new_test_data.struct_data.int_value, 33);
+}
+
+#[test]
+fn with_generic_struc_test() {
+    let struct_data = StructData {
+        int_value: 100,
+        uint_value: 200,
+    };
+    let generic_data = GenericData {
+        generic_value: 7,
+        arc_value: Arc::new(struct_data),
+    };
+    let new_generic_data = generic_data.with_generic_value(9);
+
+    assert_eq!(generic_data.generic_value, 7);
+    assert_eq!(new_generic_data.generic_value, 9);
+    assert_eq!(new_generic_data.arc_value.int_value, 100);
+}
